73e3b741c404ed9d57b1150b58b24093b5a445ba
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-01-21 19:18:46 +0100

    Add api models and migrations

---
+++ src/Support/DatabaseHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AmaizingCompany\CertifactionClient\Support;
+
+class DatabaseHelper
+{
+    public static function getTableName(string $name): string
+    {
+        return config('certifaction-client-laravel.database.table_prefix') . $name;
+    }
+}
