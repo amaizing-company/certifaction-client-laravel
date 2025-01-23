a4d2f34d4f72407ddde2fedfe17a4abd13172f99
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-01-23 14:49:43 +0100

    Add request to delete document access

---
+++ tests/Feature/Api/DeleteDocumentAccessRequestTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use AmaizingCompany\CertifactionClient\Api\Requests\DeleteDocumentAccessRequest;
+
+pest()->group('api');
+
+beforeEach(function () {
+    $this->deleteDocumentAccessRequest = new DeleteDocumentAccessRequest($this->getPdfFileContents());
+});
+
+it('can initiate instance', function () {
+    expect($this->deleteDocumentAccessRequest)
+        ->toBeInstanceOf(DeleteDocumentAccessRequest::class);
+});
+
+it('can handle file', function () {
+    expect($this->deleteDocumentAccessRequest->getFileContents())
+        ->toBeString()
+        ->toBe($this->getPdfFileContents())
+        ->and($this->deleteDocumentAccessRequest->getFileContents(true))
+        ->toBeString()
+        ->toBe(base64_encode($this->getPdfFileContents()));
+});
