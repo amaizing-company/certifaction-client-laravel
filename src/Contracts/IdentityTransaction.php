c34e002e0dfed1747f0a7ae87d759ea29a126a53
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-03-24 12:26:35 +0100

    Add missing methods and comments to IdentityTransaction contract

--- src/Contracts/IdentityTransaction.php
+++ src/Contracts/IdentityTransaction.php
@@ -25,46 +25,77 @@ interface IdentityTransaction
 {
     /**
      * Account model relationship.
+     *
+     * @return BelongsTo
      */
     public function account(): BelongsTo;
 
     /**
      * Finish the identity transaction.
+     *
+     * @param IdentificationStatus $status
+     * @param Carbon|null $lastCheck
+     * @return bool
      */
     public function finish(IdentificationStatus $status, ?Carbon $lastCheck = null): bool;
 
     /**
      * Check if the current status is failed.
+     *
+     * @return bool
      */
     public function isFailed(): bool;
 
     /**
      * Check if the current status is finished.
+     *
+     * @return bool
      */
     public function isFinished(): bool;
 
     /**
+     * Check if the current status is intent.
+     *
+     * @return bool
+     */
+    public function isIntent(): bool;
+
+    /**
      * Check if the current status is pending.
+     *
+     * @return bool
      */
     public function isPending(): bool;
 
     /**
      * Check if the current status is verified.
+     *
+     * @return bool
      */
     public function isVerified(): bool;
 
     /**
      * Set the identity transaction status to pending.
+     *
+     * @param string $identificationId
+     * @param string $identificationUrl
+     * @return bool
      */
     public function pending(string $identificationId, string $identificationUrl): bool;
 
     /**
      * Update the timestamp for last identity status checkup.
+     *
+     * @param Carbon|null $lastCheck
+     * @return bool
      */
     public function updateLastCheck(?Carbon $lastCheck = null): bool;
 
     /**
      * Update timestamp for last request.
+     *
+     * @param Carbon|null $lastRequest
+     * @return bool
      */
     public function updateLastRequest(?Carbon $lastRequest = null): bool;
 }
