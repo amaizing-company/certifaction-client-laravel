f0f597f8a5dcfc3ce8b55373653558636142514f
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-04-01 15:20:30 +0200

    Remove config call from webhook route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use AmaizingCompany\CertifactionClient\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('/signature/finish/{signatureTransaction}'), WebhookController::class)
+Route::get('/signature/finish/{signatureTransaction}', WebhookController::class)
     ->name('certifaction.webhooks.signature.finish');
